caecabdded1abe96fedb5208b41424b59f4be884
Bhanu Prakash <77429972+bhanu1031@users.noreply.github.com>
2024-07-18 09:01:52 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,14 @@
+let marks="70"
+// marks is in strings rn
+//to convert it into number we'll use
+let marks1= Number(marks)
+console.log(marks1)  // output=70
+
+let num1="20abc"
+let output1= Number(num1)
+console.log(output1);
+
+//NaN (Not a Number)
+// if null therefore= 0
+// if undefined therefore= NaN
+// if 1 therefore= true
