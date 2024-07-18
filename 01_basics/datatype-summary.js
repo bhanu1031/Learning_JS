b431da54ca88fc548872dc78ce50fc3f523b29db
Bhanu Prakash <77429972+bhanu1031@users.noreply.github.com>
2024-07-18 14:16:46 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,20 @@
+//Primitive Datatypes
+
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Non- Primitive
+// Arrays, Object, Function
+
+//Arrays
+const heroes=["Ironman", "Spiderman", "Hulk"];
+
+//Object
+let myObj={
+    name:"Bhanu",
+    age:21
+}
+
+//Fumction
+const newFun=function(){
+    console.log("Hello world");
+}
